552cc7437e890220c19e00cb2b93bdbb66b19690
DouglasFraporti <fraportidl@gmail.com>
2021-06-10 13:53:47 -0400

    material ui config and serverSide render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core'
 
 export default function Home() {
   return (
     <div>
       <h1> clone youtube com NextJS + material ui</h1>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
   )
 }
